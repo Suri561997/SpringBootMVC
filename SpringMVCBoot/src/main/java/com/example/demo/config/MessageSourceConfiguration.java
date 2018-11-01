48bddece7a514483e21cd50fd1f2fa1ec4fb3366
Suri056 <44311258+Suri056@users.noreply.github.com>
2018-11-01 12:51:33 +0530

    Update SpringMVCBoot/src/main/java/com/example/demo/config/MessageSourceConfiguration.java
    Message Source Added

--- SpringMVCBoot/src/main/java/com/example/demo/config/MessageSourceConfiguration.java
+++ SpringMVCBoot/src/main/java/com/example/demo/config/MessageSourceConfiguration.java
@@ -1,8 +1,20 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
 public class MessageSourceConfiguration {
-
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource()
+	{
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setCacheSeconds(10);
+		messageSource.setBasename("messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+	
 }
